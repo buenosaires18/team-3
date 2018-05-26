a277e013adf13f7c3a730361bd6bd00def5227b7
Santiago Moran <santiagomoran11@gmail.com>
2018-05-26 06:07:59 -0300

    arreglo chat

--- js/addNewMessage.js
+++ js/addNewMessage.js
@@ -1,17 +1,20 @@
 //get & replace to default
-const message=document.getElementById("entryMessage");
-document.getElementById("entryMessage").innerHTML+=`Escribi tu mensaje aqui`;
+function addMessage() {
 
-//Create and add element
-/*var toAddDiv =  document.createElement('div');
-toAdd.setAttribute('class', 'form-group pull-right pb-chat-labels-right' );
+    const message=document.getElementById("entryMessage");
+    message.innerHTML+=`Escribi tu mensaje aqui`;
 
-var toAddSpan = document.createElement('span');
-toAddSpan.setAttribute(class, );
+    //Create and add element
+    /*var toAddDiv =  document.createElement('div');
+    toAdd.setAttribute('class', 'form-group pull-right pb-chat-labels-right' );
 
-document.getElementById("chatBox").appendChild(toAdd);*/
+    var toAddSpan = document.createElement('span');
+    toAddSpan.setAttribute(class, );
 
-document.getElementById('form-group').innerHTML+=`<div class="form-group pull-right pb-chat-labels-right">
-<span class="label label-primary pb-chat-labels pb-chat-labels-primary">${message}</span><span class="fa fa-lg fa-user pb-chat-fa-user"></span>
-</div>`;
+    document.getElementById("chatBox").appendChild(toAdd);*/
 
+    document.getElementById('form-group').innerHTML+=`<div class="form-group pull-right pb-chat-labels-right">
+    <span class="label label-primary pb-chat-labels pb-chat-labels-primary">${message}</span><span class="fa fa-lg fa-user pb-chat-fa-user"></span>
+    </div>`;
+
+}
