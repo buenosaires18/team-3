6ae6f35a73ce50e730e68a24c5f9de25b407eee7
Santiago Moran <santiagomoran11@gmail.com>
2018-05-26 09:19:53 -0300

    Chat finalizado

--- js/addNewMessage.js
+++ js/addNewMessage.js
@@ -7,20 +7,13 @@ function addMessage() {
     let textBox = document.getElementById('entryMessage');
     let message = textBox.value
 
-    //Create and add element
-    /*var toAddDiv =  document.createElement('div');
-    toAdd.setAttribute('class', 'form-group pull-right pb-chat-labels-right' );
-
-    var toAddSpan = document.createElement('span');
-    toAddSpan.setAttribute(class, );
-
-    document.getElementById("chatBox").appendChild(toAdd);*/
-
     let nuevoMensaje = document.getElementById('nuevosMensajes').innerHTML+=`
     <hr><div style="padding-left: 30px; padding-right: 30px" class="form-group pull-right pb-chat-labels-right">
     <span class="label label-primary pb-chat-labels pb-chat-labels-primary">${message}</span><span class="fa fa-lg fa-user pb-chat-fa-user"></span>
     </div><div class="clearfix"></div>`;
 
     textBox.value = "";
+
+    
 
 }
