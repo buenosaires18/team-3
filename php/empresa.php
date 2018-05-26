336fc943228e4afb9b0032283ad938b379ef037c
Francisco Di Marzo Borghi <franciscodimarzo@hotmail.com>
2018-05-25 21:49:44 -0300

    empresa y usuario adaptados

--- php/empresa.php
+++ php/empresa.php
@@ -1,10 +1,8 @@
 <?php
 class Usuario{
     public $nombre;
-    public $password;
 
     public function __construct($nombre,$password){
         $this->nombre=$nombre;
-        $this->password=$password;
     }
 }
