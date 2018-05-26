f69a7a108135dc4e152c24103b7f8cedcf8a3258
RasT110e5 <34177449+RasT110e5@users.noreply.github.com>
2018-05-26 05:34:51 -0300

    addNewMessage

--- js/addNewMessage.js
+++ js/addNewMessage.js
@@ -1,6 +1,17 @@
 //get & replace to default
-var message=document.getElementById("entryMessage");
+const message=document.getElementById("entryMessage");
 document.getElementById("entryMessage").innerHTML+=`Escribi tu mensaje aqui`;
 
-document.createElement
+//Create and add element
+/*var toAddDiv =  document.createElement('div');
+toAdd.setAttribute('class', 'form-group pull-right pb-chat-labels-right' );
+
+var toAddSpan = document.createElement('span');
+toAddSpan.setAttribute(class, );
+
+document.getElementById("chatBox").appendChild(toAdd);*/
+
+document.getElementById('form-group').innerHTML+=`<div class="form-group pull-right pb-chat-labels-right">
+<span class="label label-primary pb-chat-labels pb-chat-labels-primary">${message}</span><span class="fa fa-lg fa-user pb-chat-fa-user"></span>
+</div>`;
 
