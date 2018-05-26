0dcb753709a01459e67bcca8d38febdfb5be25d7
Francisco Di Marzo Borghi <franciscodimarzo@hotmail.com>
2018-05-26 10:46:33 -0300

    chat fix

--- js/addNewMessage.js
+++ js/addNewMessage.js
@@ -4,8 +4,8 @@
 
 function addMessage() {
 
-    let textBox = document.getElementById('entryMessage');
-    let message = textBox.value
+    var textBox = document.getElementById('entryMessage');
+    let message = textBox.value;
 
     let nuevoMensaje = document.getElementById('nuevosMensajes').innerHTML+=`
     <hr><div style="padding-left: 30px; padding-right: 30px" >
@@ -15,5 +15,18 @@ function addMessage() {
     textBox.value = "";
 
 
+    response();
+}
+
+
+function response() {
+    setTimeout( ()=>{
+        let nuevoMensaje = document.getElementById('nuevosMensajes').innerHTML+=`
+        <hr><div style="padding-left: 30px; padding-right: 30px; background-color: rgb(253, 221, 184)" >
+        <span >Si, va a ser genial!</span><span></span>
+        </div><div class="clearfix"></div>`;
+    },3000);
+    
 
 }
+
