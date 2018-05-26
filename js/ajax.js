7f4eeb6fa0380701fd76d59c591e182e142c223a
tomasmattia <tomasjmattia@gmail.com>
2018-05-25 21:11:32 -0700

    html ready

--- js/ajax.js
+++ js/ajax.js
@@ -5,17 +5,17 @@ xmlhttp.send();
 xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         const objeto = JSON.parse(xmlhttp.responseText);
-            document.getElementById("nombre").innerHTML=`${objeto.nombre} ${objeto.apellido}`;
+            document.getElementById("nombre").innerHTML+=`${objeto.nombre} ${objeto.apellido}`;
             document.getElementById("intereses").innerHTML=`Tecnologia - Desarrollar mi carrera - Programacion`;
-            document.getElementById("nacimiento").innerHTML=`${objeto.fechaNacimiento}`;
-            document.getElementById("sexo").innerHTML=`Hombre`;
-            document.getElementById("escuela").innerHTML=`${objeto.escuela}`;
-            document.getElementById("domicilio").innerHTML=`${objeto.calle}`;
-            document.getElementById("ciudad").innerHTML=`${objeto.ciudad}`;
-            document.getElementById("provincia").innerHTML=`${objeto.provincia}`;
-            document.getElementById("celular").innerHTML=`${objeto.celular}`;
-            document.getElementById("instagram").innerHTML=`${objeto.instagram}`;
-            document.getElementById("twitter").innerHTML=`${objeto.twitter}`;
-            document.getElementById("linkedin").innerHTML=`${objeto.linkedin}`;
+            document.getElementById("nacimiento").innerHTML+=`${objeto.fechaNacimiento}`;
+            document.getElementById("sexo").innerHTML+=`Hombre`;
+            document.getElementById("escuela").innerHTML+=`${objeto.escuela}`;
+            document.getElementById("domicilio").innerHTML+=`${objeto.calle}`;
+            document.getElementById("ciudad").innerHTML+=`${objeto.ciudad}`;
+            document.getElementById("provincia").innerHTML+=`${objeto.provincia}`;
+            document.getElementById("celular").innerHTML+=`${objeto.celular}`;
+            document.getElementById("instagram").innerHTML+=`${objeto.instagram}`;
+            document.getElementById("twitter").innerHTML+=`${objeto.twitter}`;
+            document.getElementById("linkedin").innerHTML+=`${objeto.linkedin}`;
     }
 };
