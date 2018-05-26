de6c8dfdc4563b34e244e56dc16e1117e09dd8f1
lucascarrm <sandrogr0996@gmail.com>
2018-05-25 23:11:51 -0300

    Administracion pdo

--- php/administrar.php
+++ php/administrar.php
@@ -1,26 +1,22 @@
 <?php
-
+ session_start();
  $usuario='root';
  $clave='';
  $objetoPDO = new PDO('mysql:host=localhost;dbname=pescardb;charset=utf8', $usuario, $clave);
  $usuarioPost=$_POST['usuario'];
  $passPost=$_POST['password'];
 
- $sql=$objetoPDO->prepare('SELECT id,password FROM login  WHERE id');
+ $sql=$objetoPDO->prepare('SELECT id,password FROM login WHERE id=:id AND password=:password');
 
  $sql->bindParam(':id',$usuarioPost,PDO::PARAM_INT);
  $sql->bindParam(':password',$passPost,PDO::PARAM_STR);
- 
+ $sql->execute()
  while($objUsuario=$sql->fetch(PDO::FETCH_LAZY))
  {
      if($objUsuario->usuario==$usuarioPost && $objUsuario->password==$passPost){
          header('Location: localhost/team-3/index.html');
      }
-     else
-     {
-         header('Location: localhost/team-3/login/login.html');
-     }
  }
-
+ header('Location: localhost/team-3/login/login.html');
 
 ?>
