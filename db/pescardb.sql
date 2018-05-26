962910c8b9f52f73f7cf4ecd9f06d8320426a333
Francisco Di Marzo Borghi <franciscodimarzo@hotmail.com>
2018-05-26 00:02:52 -0300

    add db

--- db/pescardb.sql
+++ db/pescardb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2018 a las 05:00:28
+-- Tiempo de generación: 26-05-2018 a las 05:01:47
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
