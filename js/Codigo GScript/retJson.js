545a35f81f777de7833c83e793611424275207e4
RasT110e5 <34177449+RasT110e5@users.noreply.github.com>
2018-05-26 04:19:10 -0300

    update retJson

--- js/Codigo GScript/retJson.js
+++ js/Codigo GScript/retJson.js
@@ -19,17 +19,61 @@ function onSubmit(data){
     
     var itemResponses = data.response.getItemResponses()
     
-    Logger.log(itemResponses[0].getItem().getTitle())
-    
     itemResponses.forEach(function (item){
     
       switch(item.getItem().getTitle()){
       
-        case "" :
+        case "Luego de terminar la secundaria piensas perseguir educación superior (Universidad, tecnicatura...)" :
+          //Si / No
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Intereses" :
+          //Tecnologia / Desarrollar mi carrera / Programacion
+          Logger.log(item.getResponse())
+          break;
+        
+        case "Tal vez tengas un interés que no te mostramos" :
+          // Free entry
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Recomendarías la experiencia a otros?" :
+          //Si / No / Tal vez
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Que puntaje le daría a la experiencia": 
+          //1..5
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Agradeceríamos todos tus comentarios acerca de la experiencia con Fundación Pesca" :
+          // Free entry
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Te sentiste incluido en esta empresa?":
+          //Si / No
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Te gustaría un empleo a largo plazo en la empresa?":
+          //Si / No / Tal vez
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Como puntearías a la empresa?":
+          //1..5
+          Logger.log(item.getResponse())
+          break;
+          
+        case "Agradeceríamos todos tus comentarios acerca de la experiencia con EMPRESA":
+          // Free entry
+          Logger.log(item.getResponse())
+          break;
       
       }
-      Logger.log(item.getResponse())
-                          
     });
     
     
